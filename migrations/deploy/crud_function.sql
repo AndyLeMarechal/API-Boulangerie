2cbe477bf69326763d3f3df8c372808e2098723d
AndyLeMarechal <andy.le-marechal@oclock.school>
2024-01-19 17:46:19 +0100

    Fixe on crud_function

--- migrations/deploy/crud_function.sql
+++ migrations/deploy/crud_function.sql
@@ -114,14 +114,14 @@ CREATE OR REPLACE FUNCTION updated_breads("data" json) RETURNS breads AS $$
 	
 	UPDATE breads
 	SET
-		title = COALESCE((data->>'title'),'title'),
-        description = COALESCE((data->>'description'),'description'),
-        img = COALESCE((data->>'img'),'img'),
-        price = COALESCE((data->>'price')::numeric),
-        method_of_conservation = COALESCE((data->>'method_of_conservation'),'method_of_conservation'),
-        composition = COALESCE((data->>'composition'),'composition'),
-		nutritional_values = COALESCE((data->>'nutritional_values'),'nutritional_values'),
-		allergens = COALESCE((data->>'allergens'),'allergens'),
+		title = COALESCE((data->>'title'), title),
+        description = COALESCE((data->>'description'), description),
+        img = COALESCE((data->>'img'), img),
+        price = COALESCE((data->>'price')::numeric, price),
+        method_of_conservation = COALESCE((data->>'method_of_conservation'), method_of_conservation),
+        composition = COALESCE((data->>'composition'), composition),
+		nutritional_values = COALESCE((data->>'nutritional_values'), nutritional_values),
+		allergens = COALESCE((data->>'allergens'), allergens),
 		updated_at = now()
 	WHERE "id" = (data->>'id')::int
 RETURNING *;
@@ -131,14 +131,14 @@ CREATE OR REPLACE FUNCTION updated_pastries("data" json) RETURNS pastries AS $$
 	
 	UPDATE pastries
 	SET
-		title = COALESCE((data->>'title'),'title'),
-        description = COALESCE((data->>'description'),'description'),
-        img = COALESCE((data->>'img'),'img'),
-        price = COALESCE((data->>'price')::numeric),
-        method_of_conservation = COALESCE((data->>'method_of_conservation'),'method_of_conservation'),
-        composition = COALESCE((data->>'composition'),'composition'),
-		nutritional_values = COALESCE((data->>'nutritional_values'),'nutritional_values'),
-		allergens = COALESCE((data->>'allergens'),'allergens'),
+		title = COALESCE((data->>'title'), title),
+        description = COALESCE((data->>'description'), description),
+        img = COALESCE((data->>'img'), img),
+        price = COALESCE((data->>'price')::numeric, price),
+        method_of_conservation = COALESCE((data->>'method_of_conservation'), method_of_conservation),
+        composition = COALESCE((data->>'composition'), composition),
+		nutritional_values = COALESCE((data->>'nutritional_values'), nutritional_values),
+		allergens = COALESCE((data->>'allergens'), allergens),
 		updated_at = now()
 	WHERE "id" = (data->>'id')::int
 RETURNING *;
@@ -148,14 +148,14 @@ CREATE OR REPLACE FUNCTION updated_breakfast_pastry("data" json) RETURNS breakfa
 	
 	UPDATE breakfast_pastry
 	SET
-		title = COALESCE((data->>'title'),'title'),
-        description = COALESCE((data->>'description'),'description'),
-        img = COALESCE((data->>'img'),'img'),
-        price = COALESCE((data->>'price')::numeric),
-        method_of_conservation = COALESCE((data->>'method_of_conservation'),'method_of_conservation'),
-        composition = COALESCE((data->>'composition'),'composition'),
-		nutritional_values = COALESCE((data->>'nutritional_values'),'nutritional_values'),
-		allergens = COALESCE((data->>'allergens'),'allergens'),
+		title = COALESCE((data->>'title'), title),
+        description = COALESCE((data->>'description'), description),
+        img = COALESCE((data->>'img'), img),
+        price = COALESCE((data->>'price')::numeric, price),
+        method_of_conservation = COALESCE((data->>'method_of_conservation'), method_of_conservation),
+        composition = COALESCE((data->>'composition'), composition),
+		nutritional_values = COALESCE((data->>'nutritional_values'), nutritional_values),
+		allergens = COALESCE((data->>'allergens'), allergens),
 		updated_at = now()
 	WHERE "id" = (data->>'id')::int
 RETURNING *;
@@ -165,14 +165,14 @@ CREATE OR REPLACE FUNCTION updated_salty_side("data" json) RETURNS salty_side AS
 	
 	UPDATE salty_side
 	SET
-		title = COALESCE((data->>'title'),'title'),
-        description = COALESCE((data->>'description'),'description'),
-        img = COALESCE((data->>'img'),'img'),
-        price = COALESCE((data->>'price')::numeric),
-        method_of_conservation = COALESCE((data->>'method_of_conservation'),'method_of_conservation'),
-        composition = COALESCE((data->>'composition'),'composition'),
-		nutritional_values = COALESCE((data->>'nutritional_values'),'nutritional_values'),
-		allergens = COALESCE((data->>'allergens'),'allergens'),
+		title = COALESCE((data->>'title'), title),
+        description = COALESCE((data->>'description'), description),
+        img = COALESCE((data->>'img'), img),
+        price = COALESCE((data->>'price')::numeric, price),
+        method_of_conservation = COALESCE((data->>'method_of_conservation'), method_of_conservation),
+        composition = COALESCE((data->>'composition'), composition),
+		nutritional_values = COALESCE((data->>'nutritional_values'), nutritional_values),
+		allergens = COALESCE((data->>'allergens'), allergens),
 		updated_at = now()
 	WHERE "id" = (data->>'id')::int
 RETURNING *;
